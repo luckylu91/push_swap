6d31a913eadef260bf9cc689566c81b40711d42b
luckylu91 <zins.lucas@gmail.com>
2021-03-31 08:55:10 +0200

    merged (part 2)

--- src_pswap/orbit.c
+++ src_pswap/orbit.c
@@ -6,7 +6,7 @@
 /*   By: lzins <lzins@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/28 14:01:26 by lzins             #+#    #+#             */
-/*   Updated: 2021/03/31 08:47:11 by lzins            ###   ########lyon.fr   */
+/*   Updated: 2021/03/31 08:54:42 by lzins            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ int orbit_of(t_permut *permut, int start, t_list **orbit_lst)
 	int next;
 
 	*orbit_lst = NULL;
+	if (ft_lstdupint_back(orbit_lst, start) == -1)
+	{
+		ft_putstr_fd("Error\n", STDERR_FILENO);
+		return (-1);
+	}
 	next = permut->array[start];
 	while (next != start)
 	{
